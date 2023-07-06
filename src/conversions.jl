4227a356cf9d02ff73f580fef867d0d10c2926c3
Rafael <rafaelab@users.noreply.github.com>
2023-07-06 18:50:17 +0200

    fix problems with conversion Unitful=>Time

--- src/conversions.jl
+++ src/conversions.jl
@@ -1,4 +1,3 @@
-
 # ---------------------------------------------------------------------------------- #
 #
 @doc """
@@ -10,6 +9,80 @@ Extend Unitful's `uconvert` to enable directly counting selected units of time f
 # Output 
 . Unitful object in the desired unit of time.
 """
-Unitful.uconvert(u::Unitful.Units, t::TimeTypes) = secondsIn(t) * u"s" |> u
+uconvert(u::Unitful.Units, t::TimeTypes) = secondsIn(t) * u"s" |> u
+
+# ---------------------------------------------------------------------------------- #
+#
+@doc """
+"""
+function Base.convert(::Type{Time}, time::Unitful.Time) 
+	t = nothing
+
+	try 
+		dt = convert(Nanosecond, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	try 
+		dt = convert(Microsecond, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	try 
+		dt = convert(Millisecond, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	try 
+		dt = convert(Second, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	try 
+		dt = convert(Minute, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	try 
+		dt = convert(Hour, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	try 
+		dt = convert(Day, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	try 
+		dt = convert(Week, time)
+		t = isnothing(t) ? dt : t + dt
+		return canonicalize(t)
+	catch
+	end
+
+	if isnothing(t) 
+		throw(ErrorException("Problem resolving the time scale required."))
+	end
+
+	return t
+end
+
+
+
+
 
 # ---------------------------------------------------------------------------------- #

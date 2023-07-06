5d4c5abfc59a8c3f5283b5fd66ad2ea21e847c75
Rafael <rafaelab@users.noreply.github.com>
2023-07-06 18:50:28 +0200

    add packages

--- src/DateTimeUtils.jl
+++ src/DateTimeUtils.jl
@@ -4,9 +4,13 @@ using Reexport
 @reexport using Dates
 @reexport using Unitful 
 
+import Base: convert
+import Dates: CompoundPeriod
+import Unitful: uconvert
+
 
 include("time.jl")
 include("conversions.jl")
 
 
-end # module DateTimeUtils
+end
